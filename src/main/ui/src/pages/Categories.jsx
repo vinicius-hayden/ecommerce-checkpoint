70a5dc4091fbe7bf76ac2bd44a905288bbe4a66c
Fabiana Sato Kayanoki <89108256+FabianaYSK@users.noreply.github.com>
2022-09-27 19:34:23 -0300

    update pages Products and ProductsDetails
    useParams hook (React Router)

--- src/main/ui/src/pages/Categories.jsx
+++ src/main/ui/src/pages/Categories.jsx
@@ -1,10 +1,49 @@
 import Category from "../components/Category";
 
 export default function Categories() {
-    return (
-        <>
-            <h1 className="title" style={{ color: "#F55D15"}}>All categories:</h1>
-            <Category />
-        </>
-    );
+  /* const [categories, setCategories] = useState([]);
+
+  let requestConfigurationGet = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("token"),
+    },
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:9000/categories", requestConfigurationGet)
+      .then((response) => response.json())
+      .then((categoriesJSON) => setCategories(categoriesJSON));
+  }, []); */
+
+  return (
+    <>
+      <h1 className="title" style={{ color: "#F55D15" }}>
+        All categories:
+      </h1>
+
+    {/*<Container className="d-flex flex-column justify-content-center">
+        <Container className="col-lg-10">
+          <Row md={3}>
+            {categories.map((category, index) => (
+              <Container className="my-3" key={index}>
+                <Card className="p-2">
+                  <Card.Img src={category.image} />
+                  <Link className="nav-link" to={`/categories/${category.id}`}>
+                    <div className="text-center m-2">
+                      <Button variant="outline-dark">{category.name}</Button>
+                    </div>
+                  </Link>
+                </Card>
+              </Container>
+            ))}
+          </Row>
+        </Container>
+        </Container>*/}
+
+        <Category/>
+    </>
+  );
 }
+
+// Trocar <Category/> pelo código comentado?
--- src/main/ui/src/pages/ProductsDetails.jsx
+++ src/main/ui/src/pages/ProductsDetails.jsx
@@ -3,47 +3,63 @@ import { useParams } from "react-router-dom";
 
 import { Link } from "react-router-dom";
 
-import { Container, CardGroup, Stack, Card, Button } from "react-bootstrap";
+import { Container, Row, CardGroup, Card, Col, Button } from "react-bootstrap";
+
+import { BiArrowBack } from "react-icons/bi";
 
 export default function ProductsDetails() {
+  const { idProduct } = useParams();
+
   const [product, setProduct] = useState({});
 
   useEffect(() => {
     fetch(`http://localhost:9000/products/${idProduct}`)
       .then((response) => response.json())
-      .then((productsJSON) => setProduct(productsJSON));
+      .then((productJSON) => setProduct(productJSON));
   }, []);
-
-  const { idProduct } = useParams();
 
   return (
     <>
-      {/* Especificar os detalhes dos produtos */}
-      <h1>{idProduct}</h1>
+      {[product].map((product, index) => (
+        <Container
+          className="d-flex flex-column justify-content-center"
+          key={index}
+        >
+          <Row>
+            <Link to={"/products"}>
+              <BiArrowBack size="40" color="#F55D15" />
+            </Link>
+          </Row>
 
-      {product.map((product, index) => (
-        <Container className="my-3" key={index}>
-          <Link to={`/products/${product.id}`}>
-            <CardGroup className="col d-flex justify-content-center">
-              <Stack direction="horizontal" gap={4}>
-                <Card className="p-2">
-                  <Card.Img variant="top" src={product.image} style={{}} />
-                  <Card.Body>
-                    <Card.Title>{product.title}</Card.Title>
-                    <Card.Subtitle>{product.category.name}</Card.Subtitle>
-                    <Card.Text>{product.description}</Card.Text>
-                    <Card.Subtitle>{product.price}</Card.Subtitle>
+          <Row className="my-4">
+            <h1 style={{ color: "black" }}>{product.title}</h1>
+          </Row>
 
-                    <Link className="nav-link" to="">
-                      <div className="text-center m-2">
-                        <Button variant="outline-dark">Add to cart</Button>
-                      </div>
-                    </Link>
-                  </Card.Body>
-                </Card>
-              </Stack>
-            </CardGroup>
-          </Link>
+          <CardGroup className="col d-flex justify-content-center">
+            <Card className="w-50 p-2 border-0">
+              <Card.Img src={product.image} style={{}} />
+            </Card>
+
+            <Card className="justify-content-center border-0">
+              <Row className="mx-5 px-5 justify-content-center">
+                <Col>
+                  <h1 style={{ color: "#F55D15" }}>{product.title}</h1>
+                  <p style={{ color: "black", fontSize: "1.5rem" }}>
+                    {product.description}
+                  </p>
+                  <h4 className="mb-5">R$ {product.price}</h4>
+
+                  {/* Adicionar lógica do botão */}
+                  <Button
+                    className="btn btn-outline-primary"
+                    variant="outline-dark"
+                  >
+                    Add to Cart
+                  </Button>
+                </Col>
+              </Row>
+            </Card>
+          </CardGroup>
         </Container>
       ))}
     </>
