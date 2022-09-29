befcd6701410434c877f30fead2c54967739b669
Fabiana Sato Kayanoki <89108256+FabianaYSK@users.noreply.github.com>
2022-09-29 14:59:12 -0300

    update home (#18)
    insert div in home

--- src/main/ui/src/pages/Home.jsx
+++ src/main/ui/src/pages/Home.jsx
@@ -3,11 +3,13 @@ import Category from "../components/Category";
 
 export default function Home() {
     return (
-        <>
+       <>
+        <div className="text-center mt-5 mb-5">
             <Carousel />
-
-            <h1 className="text-center m-3" style={{ color: "#F55D15"}}>Categories:</h1>
+        </div>
+        
+        <h1 className="text-center m-3" style={{ color: "#F55D15"}}>Categories:</h1>
             <Category/>
-        </>
+       </>
     );
 }
